7ce92306dc5222ac0a37557acde9722a9b1b137c
chika <hawmea@yahoo.co.jp>
2024-03-03 14:11:16 +0900

    update: topベースコーディング

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,9 +50,42 @@ export default function Home() {
           <button>return morePortfolio;</button>
           <code>閉じタグ</code>
         </section>
+        <section>
+          <h2>私について</h2>
+          <code>const aboutMe = () =</code>
+          <div>
+            <p>
+              1988年、兵庫在住のWeb制作者です。
+              2011年に神戸のweb制作会社に6年働き、その後エイチームに転職し2024年現在7年目です。
+            </p>
+            <p>
+              前職のweb制作会社では医療・食品・教育関連など様々なweb制作の制作の傍ら
+              自社シルバーアクセサリーのECサイトの企画・運営もしていました。
+            </p>
+            <p>
+              エイチームではマーケッター・エンジニアと一緒にサイト改善を行なっています。
+              チームで働くこととフロントエンドが好きです。
+            </p>
+          </div>
+          <ul>
+            <li>
+              <button>return moreAbout;</button>
+            </li>
+            <li>
+              <button>return mySkillSet;</button>
+            </li>
+          </ul>
+          <code>閉じタグ</code>
+        </section>
       </main>
       <footer className={styles.footer}>
-        <p>これはfooterだよ</p>
+        <ul>
+          <li>TOP</li>
+          <li>Portfolio</li>
+          <li>AboutMe</li>
+          <li>MySkillSet</li>
+        </ul>
+        <small>©️CHIKA MOTOORI 2024</small>
       </footer>
     </>
   );
