e30cef853ee181c465bfc4ab49b1d706029320d3
chika <hawmea@yahoo.co.jp>
2024-03-16 16:37:22 +0900

    :art: LinkButtonにスタイルを適用

--- components/parts/LinkButton.tsx
+++ components/parts/LinkButton.tsx
@@ -1,7 +1,8 @@
 import { LinkButtonProps } from "../../types/types";
+import styles from "@styles/parts/linkButton.module.scss";
 
 function LinkButton({ children }: LinkButtonProps) {
-  return <button>{children}</button>;
+  return <button className={styles.linkButton}>{children}</button>;
 }
 
 export default LinkButton;
