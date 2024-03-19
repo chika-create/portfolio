09c2d9f7e75527e00d3626c5271972420736c755
chika <hawmea@yahoo.co.jp>
2024-03-19 09:47:37 +0900

    :recycle: snsLinksを別ファイルに定義

--- components/top/BusinessCard.tsx
+++ components/top/BusinessCard.tsx
@@ -1,11 +1,6 @@
 import businessCard from "@styles/top/businessCard.module.scss";
 import Title from "@components/parts/Title";
-
-const snsLinks = [
-  { name: "GitHub", url: "https://github.com/chika-create" },
-  { name: "Qiita", url: "https://qiita.com/chika_hoge" },
-  { name: "X", url: "https://twitter.com/chika_hoge" },
-];
+import { snsLinks } from "../../constants/snsLink";
 
 function BusinessCard() {
   return (
---
+++ constants/snsLink.ts
@@ -0,0 +1,5 @@
+export const snsLinks = [
+  { name: "GitHub", url: "https://github.com/chika-create" },
+  { name: "Qiita", url: "https://qiita.com/chika_hoge" },
+  { name: "X", url: "https://twitter.com/chika_hoge" },
+];
