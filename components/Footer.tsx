61476add08a55f8d5182dbad99abee299b821271
安井 智香 <yasui.chika@a-tm.co.jp>
2024-03-28 09:34:34 +0900

    :art: footer部分のコーディング

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,18 @@
-import styles from "@styles/footer.module.scss";
+import footerstyles from "@styles/footer.module.scss";
 
 function Footer() {
   return (
-    <footer className={styles.footer}>
-      <ul className={styles.menuList}>
-        <li className={styles.menuList__item}>TOP</li>
-        <li>Portfolio</li>
-        <li>AboutMe</li>
-        <li>MySkillSet</li>
+    <footer className={footerstyles.footer}>
+      <ul className={footerstyles.menuList}>
+        <li className={footerstyles.menuList__item}>TOP</li>
+        <li className={footerstyles.menuList__item}>Portfolio</li>
+        <li className={footerstyles.menuList__item}>AboutMe</li>
+        <li className={footerstyles.menuList__item}>MySkillSet</li>
       </ul>
-      <small>&copy; CHIKA MOTOORI 2024</small>
+      {/* TODO: 今いるメニューの箇所にmodifireを付与するロジックを追加する */}
+      <p className={footerstyles.copyright}>
+        <small>&copy; CHIKA MOTOORI 2024</small>
+      </p>
     </footer>
   );
 }
