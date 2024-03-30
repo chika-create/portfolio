f18b4c196cfe18286c7b40121f54f62cf9ed8e0b
chika <hawmea@yahoo.co.jp>
2024-03-30 16:06:18 +0900

    :+1: profileに項目を追加

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "@styles/common/page.module.scss";
 import Title from "@components/parts/Title";
 import LinkButton from "@components/parts/LinkButton";
@@ -11,22 +12,32 @@ export default function profile() {
         <section>
           <Title level="h2">私について</Title>
           <code className={styles.codeText}>const AboutMe = () = </code>
-          <dl>
-            <dt>Personal</dt>
+          <dl className={styles.profile}>
+            <dt className={styles.profile__title}>Personal</dt>
+            <dd>
+              <ul className={styles.profile__list}>
+                <li className={styles.profile__listItem}>
+                  本告 智香（モトオリ チカ）
+                </li>
+                <li className={styles.profile__listItem}>兵庫県在住</li>
+                <li className={styles.profile__listItem}>1988年11月生まれ</li>
+                <li className={styles.profile__listItem}>
+                  株式会社エイチームライフデザイン 所属
+                </li>
+                <li className={styles.profile__listItem}>
+                  <Link href="https://www.16personalities.com/ja/esfj%E5%9E%8B%E3%81%AE%E6%80%A7%E6%A0%BC">
+                    <a target="_blank" rel="noopener noreferrer">
+                      ESFJ
+                    </a>
+                  </Link>
+                </li>
+              </ul>
+              <ul>
+                <li>X</li>
+                <li>GitHub</li>
+              </ul>
+            </dd>
           </dl>
-          <dd>
-            <ul>
-              <li>本告 智香（モトオリ チカ）</li>
-              <li>兵庫県在住</li>
-              <li>1988年11月生まれ</li>
-              <li>株式会社エイチームライフデザイン 所属</li>
-              <li>ESFJ</li>
-            </ul>
-            <ul>
-              <li>X</li>
-              <li>GitHub</li>
-            </ul>
-          </dd>
           <LinkButton>return morePortfolio;</LinkButton>
           <code className={styles.codeText}>{"}"}</code>
         </section>
