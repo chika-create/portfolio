9aa64d268d68576119f64ec94d30adb31a893360
chika <hawmea@yahoo.co.jp>
2024-04-06 15:11:39 +0900

    :art: gridデザインに変更中

--- components/profile/Profile.tsx
+++ components/profile/Profile.tsx
@@ -4,9 +4,11 @@ import profileStyles from "@styles/profile/profile.module.scss";
 export const Profile = () => {
   return (
     <>
-      <dl className={profileStyles.profile}>
-        <dt className={profileStyles.profile__title}>Personal</dt>
-        <dd>
+      <div className={profileStyles.profile}>
+        <div className={profileStyles.profile__fuga}>
+          <Title level="h3">Personal</Title>
+        </div>
+        <div className={profileStyles.profile__hoge}>
           <ul className={profileStyles.profile__list}>
             <li className={profileStyles.profile__listItem}>
               本告 智香（モトオリ チカ）
@@ -32,62 +34,57 @@ export const Profile = () => {
             <li>X</li>
             <li>GitHub</li>
           </ul>
-        </dd>
-      </dl>
-      <div className={profileStyles.profile}>
-        <Title level="h3">History</Title>
-        <div>
-          <dl className={profileStyles.history}>
-            <dt className={profileStyles.history__title}>
-              <time datetime="2007-04-01">2007年</time> -{" "}
-              <time datetime="2011-03-31">2011年</time>
-              <p className={profileStyles.history__titleText}>
-                HAL大阪専門学校 WEB開発学科
-              </p>
-            </dt>
-            <dd className={profileStyles.history__text}>
-              基本情報技術者試験の資格取得やPHP、JavaScriptなどを使用しプロフラミングの基礎などを学習しました。
-            </dd>
-            <dt className={profileStyles.profile__listItem}>
-              <span className={profileStyles.profile__listItemTime}>
-                <time datetime="2011-04-01">2011年</time> -{" "}
-                <time datetime="2017-04-31">2017年</time>
-              </span>
-              <p>株式会社ドアズ</p>
-            </dt>
-            <dd>
-              Web制作とメンズシルバーアクセサリーの自社ブランドの2事業があり、
-              <br />
-              前者ではコーポレート・EC・医療・食品・自治体などの様々な種類のサイトの制作・運用を担当していました。
-              <br />
-              後者では楽天やAmazonなどのモール運営、特集ページの企画、商品企画、広告など広く担当していました。
-              <figure>
-                <img src="https://placehold.jp/412x260.png" alt="" />
-              </figure>
-            </dd>
-            <dt className={profileStyles.profile__listItem}>
-              <span className={profileStyles.profile__listItemTime}>
-                <time datetime="2017-05-01">2017年</time> -
-              </span>
-              <p>株式会社エイチームライフデザイン（現職）</p>
-            </dt>
-            <dd>
-              <p>
-                主にクレジットカードやFX、カードローンなど金融系アフィリエイトサイトの分析・改善を担当していました。
-              </p>
-              <p>
-                マーケティングの担当者と共にユーザー分析・サイト分析から改善案を考案、
-                <br />
-                デザイナーで実装しABテストを行うことでサイト改善に取り組みました。
-              </p>
-              <p>
-                またReact
-                Nativeを使用しFXのデモトレードアプリの制作に携わりました。
-                <br />
-                制作以外では小規模デザイナーチームのマネジメントを担当し、育成に力を入れました。
-              </p>
-            </dd>
-          </dl>
+        </div>
+        <div className={profileStyles.profile__fuga2}>
+          <Title level="h3">History</Title>
+        </div>
+        <p className={profileStyles.profile__hoge2}>
+          <time datetime="2007-04-01">2007年</time> -{" "}
+          <time datetime="2011-03-31">2011年</time>
+        </p>
+        <div className={profileStyles.profile__hoge3}>
+          <p className={profileStyles.history__titleText}>
+            HAL大阪専門学校 WEB開発学科
+          </p>
+          <p className={profileStyles.history__text}>
+            基本情報技術者試験の資格取得やPHP、JavaScriptなどを使用しプロフラミングの基礎などを学習しました。
+          </p>
+        </div>
+        <p className={profileStyles.profile__hoge4}>
+          <time datetime="2011-04-01">2011年</time> -{" "}
+          <time datetime="2017-04-31">2017年</time>
+        </p>
+        <div className={profileStyles.profile__hoge5}>
+          <p>株式会社ドアズ</p>
+          <p>
+            Web制作とメンズシルバーアクセサリーの自社ブランドの2事業があり、
+            <br />
+            前者ではコーポレート・EC・医療・食品・自治体などの様々な種類のサイトの制作・運用を担当していました。
+            <br />
+            後者では楽天やAmazonなどのモール運営、特集ページの企画、商品企画、広告など広く担当していました。
+            <figure>
+              <img src="https://placehold.jp/412x260.png" alt="" />
+            </figure>
+          </p>
+        </div>
+        <p className={profileStyles.profile__hoge6}>
+          <time datetime="2017-05-01">2017年</time> -
+        </p>
+        <div className={profileStyles.profile__hoge7}>
+          <p>株式会社エイチームライフデザイン（現職）</p>
+          <p>
+            主にクレジットカードやFX、カードローンなど金融系アフィリエイトサイトの分析・改善を担当していました。
+          </p>
+          <p>
+            マーケティングの担当者と共にユーザー分析・サイト分析から改善案を考案、
+            <br />
+            デザイナーで実装しABテストを行うことでサイト改善に取り組みました。
+          </p>
+          <p>
+            またReact Nativeを使用しFXのデモトレードアプリの制作に携わりました。
+            <br />
+            制作以外では小規模デザイナーチームのマネジメントを担当し、育成に力を入れました。
+          </p>
         </div>
       </div>
       <section>
