482e0c593696446e8ed64f09587e9b87dde2df2b
chika <hawmea@yahoo.co.jp>
2024-04-06 15:54:21 +0900

    :art: 大切にしていること、苦手なことのgridを設定中

--- components/profile/Profile.tsx
+++ components/profile/Profile.tsx
@@ -87,56 +87,32 @@ export const Profile = () => {
           </p>
         </div>
       </div>
-      <section>
-        <Title level="h3">大切にしていること</Title>
-        <dl className={profileStyles.profile}>
-          <dt className={profileStyles.profile__title}>Business</dt>
-          <dd>
-            <ul className={profileStyles.profile__list}>
-              <li className={profileStyles.profile__listItem}>
-                チームで一体感を持って仕事をする
-              </li>
-              <li className={profileStyles.profile__listItem}>
-                双方で配慮、尊重をする
-              </li>
-              <li className={profileStyles.profile__listItem}>
-                目的意識を持って仕事をする
-              </li>
-            </ul>
-          </dd>
-          <dt className={profileStyles.profile__title}>Create</dt>
-          <dd>
-            <ul className={profileStyles.profile__list}>
-              <li className={profileStyles.profile__listItem}>
-                品質とスピードのバランスを考える
-              </li>
-              <li className={profileStyles.profile__listItem}>
-                今後の運用を考えて開発をする
-              </li>
-            </ul>
-          </dd>
-        </dl>
-      </section>
-      <section>
-        <Title level="h3">苦手なこと</Title>
-        <dl className={profileStyles.profile}>
-          <dt className={profileStyles.profile__title}>Business</dt>
-          <dd>
-            <ul className={profileStyles.profile__list}>
-              <li className={profileStyles.profile__listItem}>
-                ビジョンを描くこと
-              </li>
-            </ul>
-          </dd>
-        </dl>
-        <dl className={profileStyles.profile}>
-          <dt className={profileStyles.profile__title}>Create</dt>
-          <dd>
-            <ul className={profileStyles.profile__list}>
-              <li className={profileStyles.profile__listItem}>デザイン</li>
-            </ul>
-          </dd>
-        </dl>
+      <section className={profileStyles.policy}>
+        <div className={profileStyles.policy__title}>
+          <Title level="h3">大切にしていること</Title>
+        </div>
+        <p className={profileStyles.policy__title__business}>Business</p>
+        <ul className={profileStyles.policy__text}>
+          <li>チームで一体感を持って仕事をする</li>
+          <li>双方で配慮、尊重をする</li>
+          <li>目的意識を持って仕事をする</li>
+        </ul>
+        <p className={profileStyles.policy__title__create}>Create</p>
+        <ul className={profileStyles.policy__text2}>
+          <li>品質とスピードのバランスを考える</li>
+          <li>今後の運用を考えて開発をする</li>
+        </ul>
+        <div className={profileStyles.notGood__title}>
+          <Title level="h3">苦手なこと</Title>
+        </div>
+        <p className={profileStyles.notGood__title__business}>Business</p>
+        <ul className={profileStyles.notGood__text}>
+          <li>ビジョンを描くこと</li>
+        </ul>
+        <p className={profileStyles.notGood__title__create}>Create</p>
+        <ul className={profileStyles.notGood__text2}>
+          <li>デザイン</li>
+        </ul>
       </section>
     </>
   );
