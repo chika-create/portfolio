d79e40fc17444504bc716c910d80a42a0b9a2c54
chika <hawmea@yahoo.co.jp>
2024-05-04 13:12:42 +0900

    :bug: 不要なファイルを削除

--- components/parts/button/Button.tsx
+++
@@ -1,6 +0,0 @@
-import { ButtonProps } from "types/types";
-import buttonStyles from "@styles/parts/button.module.scss";
-
-export const Button = ({ children }: ButtonProps) => {
-  return <button className={buttonStyles.Button}>{children}</button>;
-};
