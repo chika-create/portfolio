6354a0e284f8f0fa7349016038819ea071562485
chika <hawmea@yahoo.co.jp>
2024-05-04 12:31:44 +0900

    :bug: gitで大文字・小文字を判別するように変更

---
+++ components/PortfolioList/PortfolioList.tsx
@@ -0,0 +1,33 @@
+import { Title } from "@components/parts/Title";
+import portfolioListStyles from "@styles/portfolio/portfolioList.module.scss";
+
+export const PortfolioList = () => {
+  return (
+    <div className={portfolioListStyles.box}>
+      <div className={portfolioListStyles.content}>
+        <figure>
+          <img
+            src="https://placehold.jp/412x260.png"
+            alt=""
+            className={portfolioListStyles.image}
+          />
+        </figure>
+        <Title level="h3">ポートフォリオサイト</Title>
+        <p>
+          React, TypeScript, Next, storybook, ESLint, Prettier, Vercel, hogefuga
+        </p>
+      </div>
+      <div className={portfolioListStyles.content}>
+        <figure>
+          <img
+            src="https://placehold.jp/412x260.png"
+            alt=""
+            className={portfolioListStyles.image}
+          />
+        </figure>
+        <Title level="h3">おにぎり計算機</Title>
+        <p>React, TypeScript, MUI, ESLint, Prettier, Vercel</p>
+      </div>
+    </div>
+  );
+};
