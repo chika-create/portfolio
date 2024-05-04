8cc31d036d0dbbc63d77110ea342eeffd3ad9c5c
chika <hawmea@yahoo.co.jp>
2024-05-04 11:54:21 +0900

    :sparkles: skillページのコンポーネントを追加

--- pages/skill/index.tsx
+++ pages/skill/index.tsx
@@ -2,6 +2,7 @@ import styles from "@styles/common/page.module.scss";
 import { Title } from "@components/parts/Title/";
 import { Button } from "@components/parts/Button/";
 import { Footer } from "@components/Footer/";
+import { Skill } from "@components/Skill";
 
 export default function profile() {
   return (
@@ -11,6 +12,7 @@ export default function profile() {
         <section>
           <Title level="h2">私について</Title>
           <code className={styles.codeText}>const AboutMe = () = </code>
+          <Skill />
           <Button>return morePortfolio;</Button>
           <code className={styles.codeText}>{"}"}</code>
         </section>
