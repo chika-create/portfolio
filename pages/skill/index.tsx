2f2c390bac5ae54925753f76609ea93358dbc812
chika <hawmea@yahoo.co.jp>
2024-05-04 11:46:43 +0900

    :art: スキルページを追加

---
+++ pages/skill/index.tsx
@@ -0,0 +1,21 @@
+import styles from "@styles/common/page.module.scss";
+import { Title } from "@components/parts/Title/";
+import { Button } from "@components/parts/Button/";
+import { Footer } from "@components/Footer/";
+
+export default function profile() {
+  return (
+    <>
+      <header className={styles.header}>hoge</header>
+      <main className={styles.main}>
+        <section>
+          <Title level="h2">私について</Title>
+          <code className={styles.codeText}>const AboutMe = () = </code>
+          <Button>return morePortfolio;</Button>
+          <code className={styles.codeText}>{"}"}</code>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+}
