67d76225c03d2d61aaedc08db1eb6c3414b4b7ea
chika <hawmea@yahoo.co.jp>
2025-08-16 11:34:59 +0900

    :recycle: value周りの冗長な命名を修正

--- components/Profile/ProfileValuesAndChallenges.tsx
+++ components/Profile/ProfileValuesAndChallenges.tsx
@@ -4,18 +4,18 @@ import profileStyles from "@styles/profile/profile.module.scss";
 export const ProfileValuesAndChallenges = () => {
   return (
     <>
-      <section className={profileStyles[`profile--value`]}>
-        <div className={profileStyles[`profile--value__title`]}>
+      <section className={profileStyles[`value`]}>
+        <div className={profileStyles[`value__title`]}>
           <Title level="h3">大切にしていること</Title>
         </div>
-        <p className={profileStyles[`profile__value__title--business`]}>Business</p>
-        <ul className={profileStyles[`profile__value--business`]}>
+        <p className={profileStyles[`value__title--business`]}>Business</p>
+        <ul className={profileStyles[`value--business`]}>
           <li>チームで一体感を持って仕事をする</li>
           <li>双方で配慮、尊重をする</li>
           <li>目的意識を持って仕事をする</li>
         </ul>
-        <p className={profileStyles[`profile__value__title--create`]}>Create</p>
-        <ul className={profileStyles[`profile__value--create`]}>
+        <p className={profileStyles[`value__title--create`]}>Create</p>
+        <ul className={profileStyles[`value--create`]}>
           <li>品質とスピードのバランスを考える</li>
           <li>今後の運用を考えて開発をする</li>
         </ul>
