962d804bf9a36d6393971ae4f3142efca4f3e0d1
chika <hawmea@yahoo.co.jp>
2026-02-09 14:13:06 +0900

    :recycle: exportするコンポーネント名を大文字に変更、それに伴いインポートするコンポーネント名のエイリアスを設定

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -2,9 +2,9 @@ import pageStyles from "@styles/common/page.module.scss";
 import { Title } from "@components/parts/Title";
 import { Button } from "@components/parts/Button";
 import { Footer } from "@components/Footer";
-import { Profile } from "@components/Profile";
+import { Profile as ProfileComponent } from "@components/Profile";
 
-export default function profile() {
+export default function Profile() {
   return (
     <>
       <header className={pageStyles.header}>hoge</header>
@@ -12,7 +12,7 @@ export default function profile() {
         <section>
           <Title level="h2">私について</Title>
           <code className={pageStyles.codeText}>const AboutMe = () ={'>'} </code>
-          <Profile />
+          <ProfileComponent />
           <Button>return TOP;</Button>
           <code className={pageStyles.codeText}>{"}"}</code>
         </section>
