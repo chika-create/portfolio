6dec59def54446691e22ce24977f1f273912a9d2
chika <hawmea@yahoo.co.jp>
2026-02-10 14:04:20 +0900

    :+1: Footer のナビをリンクに

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,13 +1,22 @@
+import Link from "next/link";
 import footerStyles from "@styles/footer.module.scss";
 
 export const Footer = () => {
   return (
     <footer className={footerStyles.footer}>
       <ul className={footerStyles.menuList}>
-        <li className={footerStyles.menuList__item}>TOP</li>
-        <li className={footerStyles.menuList__item}>Portfolio</li>
-        <li className={footerStyles.menuList__item}>AboutMe</li>
-        <li className={footerStyles.menuList__item}>MySkillSet</li>
+        <Link href="/">
+          <li className={footerStyles.menuList__item}>TOP</li>
+        </Link>
+        <Link href="/portfolio">
+          <li className={footerStyles.menuList__item}>Portfolio</li>
+        </Link>
+        <Link href="/profile">
+          <li className={footerStyles.menuList__item}>AboutMe</li>
+        </Link>
+        <Link href="/skill">
+          <li className={footerStyles.menuList__item}>MySkillSet</li>
+        </Link>
       </ul>
       {/* TODO: 今いるメニューの箇所にmodifireを付与するロジックを追加する */}
       <p className={footerStyles.copyright}>
