208ee5a7e27a2225a0b2e4e350269c0637deaf5a
chika <hawmea@yahoo.co.jp>
2026-06-06 15:44:58 +0900

    ♻ スキルデータの構造を改善し、アイコンと色を動的に取得するように変更

--- components/Top/topSkillData.ts
+++ components/Top/topSkillData.ts
@@ -1,38 +1,31 @@
-import {
-  SiReact,
-  SiNextdotjs,
-  SiJavascript,
-  SiTypescript,
-  SiGit,
-  SiHtml5,
-  SiCss,
-  SiStorybook,
-  SiEslint,
-  SiPrettier,
-  SiGithubcopilot,
-  SiVercel,
-  SiFigma,
-  SiGoogleanalytics,
-} from "react-icons/si";
 import { TopSkillItem } from "types/types";
+import { skillIconColors, skillIcons } from "@components/Skill/skillIconMap";
 
-export const topSkillSet: TopSkillItem[] = [
-  { name: "React", color: "#61DAFB", Icon: SiReact },
-  { name: "Next.js", color: "#111111", Icon: SiNextdotjs },
-  { name: "JavaScript", color: "#F7DF1E", Icon: SiJavascript },
-  { name: "TypeScript", color: "#3178C6", Icon: SiTypescript },
-  { name: "Git", color: "#F05032", Icon: SiGit },
-  { name: "HTML", color: "#E34F26", Icon: SiHtml5 },
-  { name: "CSS", color: "#1572B6", Icon: SiCss },
-  { name: "Storybook", color: "#FF4785", Icon: SiStorybook },
-  { name: "ESLint", color: "#4B32C3", Icon: SiEslint },
-  { name: "Prettier", color: "#F7B93E", Icon: SiPrettier },
-  { name: "Copilot", color: "#0EA5E9", Icon: SiGithubcopilot },
-  { name: "Vercel", color: "#111111", Icon: SiVercel },
-  { name: "Figma", color: "#F24E1E", Icon: SiFigma },
-  {
-    name: "Google Analytics",
-    color: "#E37400",
-    Icon: SiGoogleanalytics,
-  },
-];
+const topSkillSourceNames = [
+  "React",
+  "Next.js",
+  "JavaScript",
+  "TypeScript",
+  "Git / GitHub / GitLab",
+  "HTML",
+  "CSS",
+  "Storybook",
+  "ESLint",
+  "Prettier",
+  "Copilot",
+  "Vercel",
+  "Figma",
+  "Google Analytics",
+] as const;
+
+type TopSkillSourceName = (typeof topSkillSourceNames)[number];
+
+const topSkillLabelOverrides: Partial<Record<TopSkillSourceName, string>> = {
+  "Git / GitHub / GitLab": "Git",
+};
+
+export const topSkillSet: TopSkillItem[] = topSkillSourceNames.map((sourceName) => ({
+  name: topSkillLabelOverrides[sourceName] ?? sourceName,
+  color: skillIconColors[sourceName],
+  Icon: skillIcons[sourceName],
+}));
