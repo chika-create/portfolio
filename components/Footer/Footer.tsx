286caf06faf530467bb571da52960ff71b819827
chika <hawmea@yahoo.co.jp>
2026-06-07 15:21:28 +0900

    SPのスタイル調整、PC版の横幅の数字を定数化など

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,24 +4,26 @@ import footerStyles from "@styles/footer.module.scss";
 export const Footer = () => {
   return (
     <footer className={footerStyles.footer}>
-      <ul className={footerStyles.menuList}>
-        <li className={footerStyles.menuList__item}>
-          <Link href="/">TOP</Link>
-        </li>
-        <li className={footerStyles.menuList__item}>
-          <Link href="/portfolio">Projects</Link>
-        </li>
-        <li className={footerStyles.menuList__item}>
-          <Link href="/profile">About</Link>
-        </li>
-        <li className={footerStyles.menuList__item}>
-          <Link href="/skill">SkillSet</Link>
-        </li>
-      </ul>
-      {/* TODO: 今いるメニューの箇所にmodifireを付与するロジックを追加する？ */}
-      <p className={footerStyles.copyright}>
-        <small>&copy; CHIKA MOTOORI {new Date().getFullYear()}</small>
-      </p>
+      <div className={footerStyles.footerInner}>
+        <ul className={footerStyles.menuList}>
+          <li className={footerStyles.menuList__item}>
+            <Link href="/">TOP</Link>
+          </li>
+          <li className={footerStyles.menuList__item}>
+            <Link href="/portfolio">Projects</Link>
+          </li>
+          <li className={footerStyles.menuList__item}>
+            <Link href="/profile">About</Link>
+          </li>
+          <li className={footerStyles.menuList__item}>
+            <Link href="/skill">SkillSet</Link>
+          </li>
+        </ul>
+        {/* TODO: 今いるメニューの箇所にmodifireを付与するロジックを追加する？ */}
+        <p className={footerStyles.copyright}>
+          <small>&copy; CHIKA MOTOORI {new Date().getFullYear()}</small>
+        </p>
+      </div>
     </footer>
   );
 };
